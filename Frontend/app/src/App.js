ddf65282a8387a56b5d0640981f01d9fc7b43411
AHdev22 <amr.h.dev22@gmail.com>
2025-10-10 13:41:51 +0300

    Apply Frontend Structure

---
+++ Frontend/app/src/App.js
@@ -0,0 +1,28 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+    const token = localStorage.getItem("token");
+
+    return (
+        <Router>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                    path="/"
+                    element={token ? <Dashboard /> : <Navigate to="/login" />}
+                />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
